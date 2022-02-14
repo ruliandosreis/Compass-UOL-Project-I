0a768d0dc53743a7ca5df88f3586ad1f691f6d97
Rulian Dos Reis <rulian.dosreis@hotmail.com>
2022-02-14 08:39:42 -0300

    Feat: Cleaning fields when they are not validated

--- js/formValidation.js
+++ js/formValidation.js
@@ -7,6 +7,7 @@ form.addEventListener("submit", event =>{
     isPasswordValid();
     isPhoneValid();
 
+
     if(isChecked() == true && isNameValid() == true && isEmailValid() == true && isPasswordValid() == true && isPhoneValid() == true){
         location.href="sucesspage.html";
     }
@@ -15,24 +16,27 @@ form.addEventListener("submit", event =>{
 
 function isChecked(){
     const checkbox = document.querySelector("#checkmark");
+    const checkWarning = document.getElementById("check-warning");
     if(!checkbox.checked){
-        const checkWarning = document.getElementById("check-warning");
         checkWarning.classList.remove("invisible");
         return false;
     }
     else{
+        checkWarning.classList.add("invisible");
         return true;
     }
 }
 
 function isNameValid(){
     const name = document.querySelector("#full-name");
+    const nameWarning = document.getElementById("name-warning");
     if(name.value.length>4 && name.value.indexOf(" ")!=(-1)){
+        nameWarning.classList.add("invisible");
         return true;
     }
     else{
-        const nameWarning = document.getElementById("name-warning");
         nameWarning.classList.remove("invisible");
+        name.value="";
         return false;
     }
 }
@@ -40,24 +44,29 @@ function isNameValid(){
 function isEmailValid(){
     const email = document.querySelector("#email");
     const emailWarning = document.getElementById("email-warning");
-    const emailRegex = /^[a-z0-9.]+@+[a-z0-9.]+$/;
+    const emailRegex = /^[a-z0-9.]+@+[a-z0-9. ]+$/;
 
     if(!emailRegex.test(email.value)) {
         emailWarning.classList.remove("invisible");
+        email.value="";
         return false;
+    }else{
+        emailWarning.classList.add("invisible");
+        return true;
     }
-    return true;
 }
 
 function isPhoneValid(){
     const phone = document.querySelector("#phonenumber");
     const phoneWarning = document.getElementById("phone-warning");
-    const phoneRegex = /^[0-9]+$/;
+    const phoneRegex = /^[0-9() -]+$/;
 
-    if(phoneRegex.test(phone.value) && phone.value.length == 11){
+    if(phoneRegex.test(phone.value) && phone.value.length > 11){
+        phoneWarning.classList.add("invisible");
         return true;
     }else{
         phoneWarning.classList.remove("invisible");
+        phone.value="";
         return false;
     }
 
@@ -70,9 +79,11 @@ function isPasswordValid(){
     
 
     if(passwordRegex.test(password.value) && password.value.length>=6 && password.value.length<=9){
+        passwordWarning.classList.add("invisible");
         return true;
     }else{
         passwordWarning.classList.remove("invisible");
+        password.value="";
         return false;
     }
 
